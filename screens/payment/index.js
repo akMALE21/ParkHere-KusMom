061629794df12f3461cdc13d5a5cc6a54fd77e5a
Rayhan Naufal Luthfi <91775520+rayhannaufall10@users.noreply.github.com>
2023-05-25 22:21:24 +0700

    Fix Payment Page

--- screens/payment/index.js
+++ screens/payment/index.js
@@ -13,10 +13,46 @@ export default function Payment() {
     const navigation = useNavigation();
     const { user } = useAuth();
     const [loading, setLoading] = useState(true);
-    const [selectedOption, setSelectedOption] = useState(null);
+    const [selectedOption, setSelectedOption] = useState(1);
 
     const handleOptionSelect = (option) => {
         setSelectedOption(option);
+    };
+
+    const getImageDimensions = (option) => {
+        // Define the image dimensions based on the selected option
+        switch (option) {
+          case 1:
+            return { width: 74, height: 42, marginLeft: 15, marginTop: 10 };
+          case 2:
+            return { width: 70, height: 20, marginLeft: 15, marginTop: 20 };
+          case 3:
+            return { width: 74, height: 25, marginLeft: 13, marginTop: 15 };
+          case 4:
+            return { width: 80, height: 42, marginLeft: 15, marginTop: 8 };
+          case 5:
+            return { width: 80, height: 42, marginLeft: 14, marginTop: 8 };
+          default:
+            return { width: 0, height: 0 };
+        }
+    };
+
+    const getImageSource = (option) => {
+        // Define the image source based on the selected option
+        switch (option) {
+          case 1:
+            return require("../../assets/img/gopay.png");
+          case 2:
+            return require("../../assets/img/danaLogo.png");
+          case 3:
+            return require("../../assets/img/ovoLogo.png");
+          case 4:
+            return require("../../assets/img/shopeepayLogo.png");
+          case 5:
+            return require("../../assets/img/linkajaLogo.png");
+          default:
+            return null;
+        }
     };
 
     return <SafeAreaView style={styles.container}>
@@ -36,17 +72,17 @@ export default function Payment() {
                 <View style={styles.profileTextContainer}>
                     <Text style={styles.profileText}>Payment</Text>
                 </View>
-                <View style={styles.imageContainer}>
-
-                </View>
+                <View style={styles.imageContainer}></View>
             </View>
 
             <View style={styles.Payment}>
                 <View style={styles.usePay}>
-                    <Image
-                        source={require('../../assets/img/gopay.png')}
-                        style={{ width: 74, height: 42, marginLeft: 15, marginTop: 10 }}
-                    />
+                    {selectedOption && (
+                        <Image
+                            source={getImageSource(selectedOption)}
+                            style={getImageDimensions(selectedOption)}
+                        />
+                    )}
                     <View style={{ flexDirection: "row", allignItems: "center" }}>
                         <Text style={{ marginTop: 20, fontSize: 10 }}>IDR</Text>
                         <Text style={{ marginTop: 20, fontSize: 18 }}>50.000</Text>
@@ -56,46 +92,53 @@ export default function Payment() {
         </View>
         <View style={styles.mainContainer}>
             <Text style={styles.methodText}>Choose Payment Method</Text>
-            <TouchableOpacity 
-                    style={[styles.paymentOption, selectedOption === 1 ? styles.selectedOption : null]}
-                    onPress={() => handleOptionSelect(1)}>
-                    <Image
-                        source={require('../../assets/img/gopay.png')}
-                        style={{ width: 74, height: 42, marginLeft: 15, marginTop: 10 }}
-                    />
-            </TouchableOpacity>
-            <TouchableOpacity 
-                style={[styles.paymentOption, selectedOption === 2 ? styles.selectedOption : null]}
-                onPress={() => handleOptionSelect(2)}>
-                <Image
-                    source={require('../../assets/img/gopay.png')}
-                    style={{ width: 74, height: 42, marginLeft: 15, marginTop: 10 }}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity 
-                style={[styles.paymentOption, selectedOption === 3 ? styles.selectedOption : null]}
-                onPress={() => handleOptionSelect(3)}>
-                <Image
-                    source={require('../../assets/img/gopay.png')}
-                    style={{ width: 74, height: 42, marginLeft: 15, marginTop: 10 }}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity 
-                style={[styles.paymentOption, selectedOption === 4 ? styles.selectedOption : null]}
-                onPress={() => handleOptionSelect(4)}>
-                <Image
-                    source={require('../../assets/img/gopay.png')}
-                    style={{ width: 74, height: 42, marginLeft: 15, marginTop: 10 }}
-                />
-            </TouchableOpacity>
-            <TouchableOpacity 
-                style={[styles.paymentOption, selectedOption === 5 ? styles.selectedOption : null]}
-                onPress={() => handleOptionSelect(5)}>
-                <Image
-                    source={require('../../assets/img/gopay.png')}
-                    style={{ width: 74, height: 42, marginLeft: 15, marginTop: 10 }}
-                />
-            </TouchableOpacity>
+            <View style={styles.paymentOptions}>
+        <TouchableOpacity
+          style={[styles.paymentOption, selectedOption === 1 ? styles.selectedOption : null]}
+          onPress={() => handleOptionSelect(1)}
+        >
+          <Image
+            source={require("../../assets/img/gopay.png")}
+            style={{ width: 90, height: 42, marginLeft: 3, marginTop: 5 }}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.paymentOption, selectedOption === 2 ? styles.selectedOption : null]}
+          onPress={() => handleOptionSelect(2)}
+        >
+          <Image
+            source={require("../../assets/img/danaLogo.png")}
+            style={styles.paymentOptionImage}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.paymentOption, selectedOption === 3 ? styles.selectedOption : null]}
+          onPress={() => handleOptionSelect(3)}
+        >
+          <Image
+            source={require("../../assets/img/ovoLogo.png")}
+            style={styles.paymentOptionImage}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.paymentOption, selectedOption === 4 ? styles.selectedOption : null]}
+          onPress={() => handleOptionSelect(4)}
+        >
+          <Image
+            source={require("../../assets/img/shopeepayLogo.png")}
+            style={styles.paymentOptionImage}
+          />
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={[styles.paymentOption, selectedOption === 5 ? styles.selectedOption : null]}
+          onPress={() => handleOptionSelect(5)}
+        >
+          <Image
+            source={require("../../assets/img/linkajaLogo.png")}
+            style={styles.paymentOptionImage}
+          />
+        </TouchableOpacity>
+      </View>
         </View>
     </SafeAreaView>
 }
@@ -147,7 +190,8 @@ export default function Payment() {
             height: 60,
             borderRadius: 16,
             marginLeft: 10,
-            paddingHorizontal: 15,
+            paddingRight: 15,
+            paddingLeft: 5,
         },
         mainContainer: {
             flex: 1,
@@ -163,20 +207,27 @@ export default function Payment() {
             fontWeight: "semibold",
             paddingBottom: 30,
         },
+        paymentOptions: {
+            paddingHorizontal: 20,
+        },
         paymentOption: {
             flexDirection: "row",
             justifyContent: "space-between",
+            alignItems: "center",
             backgroundColor: "white",
-            width: 350,
             height: 60,
             borderRadius: 16,
-            paddingHorizontal: 15,
             marginBottom: 20,
             borderWidth: 1,
             borderColor: "black",
+            paddingHorizontal: 15,
+        },
+        paymentOptionImage: {
+            aspectRatio: 1,
+            resizeMode: "contain",
         },
         selectedOption: {
             borderColor: "#5995F1",
-        },
+        }
     }
 );
