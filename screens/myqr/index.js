591107751f81052319fe1fee3fd7d003656ec436
akMALE21 <akmalmunggaran.kerja@gmail.com>
2023-05-26 04:36:30 +0700

    fix page my qr

--- screens/myqr/index.js
+++ screens/myqr/index.js
@@ -1,7 +1,12 @@
 import { FlatList, StyleSheet, View, Alert, TouchableOpacity, ScrollView } from "react-native";
-import { ActivityIndicator, Appbar, Button, FAB, IconButton, List, Text } from "react-native-paper";
+import { ActivityIndicator, Appbar, Button, FAB, IconButton, List, Text, Divider, Menu } from "react-native-paper";
+import { Dropdown } from 'react-native-element-dropdown';
+import AntDesign from '@expo/vector-icons/AntDesign';
+import { MaterialCommunityIcons } from "@expo/vector-icons";
 import theme from "../../config/theme";
-import DropdownPicker from 'react-native-dropdown-picker'; // Import komponen DropdownPicker
+import RNPickerSelect from "react-native-picker-select";
+import DropdownPicker from 'react-native-dropdown-picker';
+
 
 import auth from '@react-native-firebase/auth';
 import { useNavigation } from "@react-navigation/native";
@@ -17,32 +22,132 @@ export default function MyQR() {
     const navigation = useNavigation();
     const { user } = useAuth();
     const [loading, setLoading] = useState(true);
+    const [value, setValue] = useState(null);
 
-    const handleLogout = () => {
-        auth().signOut();
-    }
+    const data = [
+        { label: 'Mercedes G 63', value: '1' },
+        { label: 'Item 2', value: '2' },
+        { label: 'Item 3', value: '3' },
+        { label: 'Item 4', value: '4' },
+        { label: 'Item 5', value: '5' },
+        { label: 'Item 6', value: '6' },
+        { label: 'Item 7', value: '7' },
+        { label: 'Item 8', value: '8' },
+    ];
 
-    const handleDropdownChange = (itemValue) => {
-        // Lakukan sesuatu ketika nilai dropdown berubah
+    const renderItem = item => {
+        return (
+            <View style={styles.item}>
+                <Text style={styles.textItem}>{item.label}</Text>
+                {item.value === value && (
+                    <AntDesign
+                        style={styles.icon}
+                        color="black"
+                        name="Safety"
+                        size={20}
+                    />
+                )}
+            </View>
+        );
     };
 
     return <SafeAreaView style={styles.container}>
-        <View style={styles.header}>
-            {/* Konten header */}
+        <View style={styles.headerContainer}>
+            <View style={styles.headerContent}>
+                <View style={styles.profileTextContainer}>
+                    <Text style={styles.profileText}>My QR</Text>
+                </View>
+            </View>
         </View>
+        <Dropdown
+            style={styles.dropdown}
+            placeholderStyle={styles.placeholderStyle}
+            selectedTextStyle={styles.selectedTextStyle}
+            inputSearchStyle={styles.inputSearchStyle}
+            iconStyle={styles.iconStyle}
+            data={data}
+            search
+            maxHeight={300}
+            labelField="label"
+            valueField="value"
+            placeholder="Select Vehicle"
+            searchPlaceholder="Search..."
+            value={value}
+            onChange={item => {
+                setValue(item.value);
+            }}
+            renderLeftIcon={() => (
+                <AntDesign style={styles.icon} color="black" name="Safety" size={20} />
+            )}
+            renderItem={renderItem}
+        />
         <View style={styles.main}>
-            {/* Konten main */}
-            <View style={styles.dropdownContainer}>
-                <DropdownPicker
-                    items={[
-                        { label: 'Option 1', value: 'option1' },
-                        { label: 'Option 2', value: 'option2' },
-                        { label: 'Option 3', value: 'option3' },
-                    ]}
-                    defaultValue={null}
-                    containerStyle={styles.dropdown}
-                    onChangeItem={(item) => handleDropdownChange(item.value)}
-                />
+            <View style={styles.box}>
+                <View style={styles.boxQR}>
+                    <Text style={styles.boxQRText}>
+                        Scan this on the scanner machine when you are in the parking lot
+                    </Text>
+                    <Image
+                        source={require("../../assets/img/frameqr.png")}
+                        style={styles.imageQR}
+                    />
+                </View>
+                <View style={{borderBottomWidth: 1,borderStyle:"dashed", height:1, width:330, alignSelf: "center"}}>
+                </View>
+                <View style={styles.boxKet}>
+                    <View style={styles.boxAtas}>
+                        <View style={styles.boxKiri}>
+                            <View style={styles.boxJudul}>
+                                <Text style={styles.boxQRJudul}>
+                                    Vehicle Number
+                                </Text>
+                            </View>
+                            <View style={styles.boxIsi}>
+                                <Text style={styles.boxQRIsi}>
+                                    D 100 AF
+                                </Text>
+                            </View>
+                        </View>
+                        <View style={styles.boxKanan}>
+                            <View style={styles.boxJudul}>
+                                <Text style={styles.boxQRJudul}>
+                                    STNK NUMBER
+                                </Text>
+                            </View>
+                            <View style={styles.boxIsi}>
+                                <Text style={styles.boxQRIsi}>
+                                    11245688
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                    <View style={styles.boxBawah}>
+                        <View style={styles.boxKiri}>
+                            <View style={styles.boxJudul}>
+                                <Text style={styles.boxQRJudul}>
+                                    Brand
+                                </Text>
+                            </View>
+                            <View style={styles.boxIsi}>
+                                <Text style={styles.boxQRIsi}> 
+                                    Mercedes G 63
+                                </Text>
+                            </View>
+                        </View>
+                        <View style={styles.boxKanan}>
+                            <View style={styles.boxJudul}>
+                                <Text style={styles.boxQRJudul}>
+                                    Type
+                                </Text>
+                            </View>
+                            <View style={styles.boxIsi}>
+                                <Text style={styles.boxQRIsi}>
+                                    Car
+                                </Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
             </View>
         </View>
         <View style={styles.component}>
@@ -98,20 +203,144 @@ export default function MyQR() {
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: "rgb(89, 149, 241)",
-        flex: 1
-    },
-    header: {
         flex: 1,
+        backgroundColor: "#F8F8F8",
+    },
+    headerContainer: {
+        backgroundColor: "#5995F1",
+        height: 166,
         flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        borderBottomLeftRadius: 30,
+        borderBottomRightRadius: 30,
+    },
+    profileTextContainer: {
+        marginBottom: 50,
+    },
+    profileText: {
+        color: "white",
+        fontSize: 32,
+        fontWeight: "bold",
+    },
+    dropdown: {
+        bottom: 50,
+        posistion: "absolute",
+        left: 30,
+        bottom: 25,
+        height: 50,
+        width: 350,
+        backgroundColor: 'white',
+        borderRadius: 12,
+        padding: 12,
+        shadowColor: '#000',
+        shadowOffset: {
+            width: 0,
+            height: 1,
+        },
+        shadowOpacity: 0.2,
+        shadowRadius: 1.41,
 
+        elevation: 2,
+    },
+    icon: {
+        marginRight: 5,
+    },
+    item: {
+        padding: 17,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+    },
+    textItem: {
+        flex: 1,
+        fontSize: 16,
+    },
+    placeholderStyle: {
+        fontSize: 16,
+    },
+    selectedTextStyle: {
+        fontSize: 16,
+    },
+    iconStyle: {
+        width: 20,
+        height: 20,
+    },
+    inputSearchStyle: {
+        height: 40,
+        fontSize: 16,
     },
     main: {
-        flex: 5,
+        flex: 1,
         backgroundColor: "#F8F8F8",
-        borderTopLeftRadius: 30,
-        borderTopRightRadius: 30,
-        paddingBottom: 50
+    },
+    box: {
+        flex: 1,
+        marginTop: 20,
+        marginLeft: 20,
+        marginRight: 20,
+        paddingBottom: 120,
+        height: 2,
+    },
+    boxQR: {
+        flex: 2,
+        backgroundColor: "white",
+        alignItems: "center",
+        borderRadius: 25,
+    },
+    boxQRText: {
+        color: "black",
+        fontSize: 18,
+        fontWeight: "medium",
+        textAlign: "center",
+        width: 280,
+        margin: 15,
+    },
+    imageQR: {
+        width: 226,
+        height: 226,
+        marginTop: 15,
+    },
+    boxKet: {
+        flex: 1,
+        backgroundColor: "white",
+        borderRadius: 25,
+        justifyContent: "center",
+    },
+    boxAtas:{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        marginBottom: 5,
+    },
+    boxKiri:{
+        width: 150,
+        height: 70,
+        justifyContent: "center",
+        marginLeft: 20
+    },
+    boxKanan:{
+        width: 150,
+        height: 70,
+        justifyContent: "center",
+        marginRight: 15    
+    },
+    boxBawah:{
+        flexDirection: "row",
+        justifyContent: "space-between",
+        
+    },
+    boxJudul:{
+        marginBottom: 3
+    },
+    boxQRJudul: {
+        color: "black",
+        fontSize: 18,
+        fontWeight: "medium",
+    },
+    boxQRIsi: {
+        color: "black",
+        fontSize: 20,
+        fontWeight: "bold",
     },
     component: {
         flex: 1,
@@ -161,13 +390,5 @@ const styles = StyleSheet.create({
         justifyContent: "center",
         alignItems: "center",
     },
-    dropdownContainer: {
-        marginTop: 20,
-        alignSelf: 'center',
-    },
-    dropdown: {
-        backgroundColor: '#ffffff',
-        width: 200,
-        height: 40,
-    },
 })
+
